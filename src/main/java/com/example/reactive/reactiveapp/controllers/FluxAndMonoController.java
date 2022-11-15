ed3006809069b7d7bb20f47280eafdf886bdcff7
Juan Antonio <juanantonio.cx@gmail.com>
2022-11-15 00:57:50 -0500

    📦 NEW: add examples endppints

---
+++ src/main/java/com/example/reactive/reactiveapp/controllers/FluxAndMonoController.java
@@ -0,0 +1,29 @@
+package com.example.reactive.reactiveapp.controllers;
+
+import java.time.Duration;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> flux() {
+        return Flux.just(1, 2, 3, 4).log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> helloWorlMono() {
+        return Mono.just("hello mono").log();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofSeconds(1)).log();
+    }
+}
